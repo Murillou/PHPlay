edc01a0256f48875774115501609fda51e3cc484
Murillou <murillovinicius13@hotmail.com>
2025-03-12 02:14:43 -0300

    chore: implementar segurança ao obter parâmetros

--- pages/new-video.php
+++ pages/new-video.php
@@ -2,11 +2,21 @@
 
   require '../config/connection-bd.php';
 
-$sqlQuery = 'INSERT INTO videos (url, title) VALUES (:url, :title);';
+$url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
+if ($url === false) {
+  header('Location: ../index.php?sucesso=0');
+  exit();
+}
+$title = filter_input(INPUT_POST, 'title');
+if ($title === false) {
+  header('Location: ../index.php?sucesso=0');
+  exit();
+}
 
+$sqlQuery = 'INSERT INTO videos (url, title) VALUES (:url, :title);';
 $stmt = $pdo->prepare($sqlQuery);
-$stmt->bindValue(':url', $_POST['url'], PDO::PARAM_STR);
-$stmt->bindValue(':title', $_POST['title'], PDO::PARAM_STR);
+$stmt->bindValue(':url', $url, PDO::PARAM_STR);
+$stmt->bindValue(':title', $title, PDO::PARAM_STR);
 
 if ($stmt->execute() ===false) {
   header('Location: ../index.php?sucesso=0');
