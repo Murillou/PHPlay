ca538c29a396926f403376cf3f5356a91deca502
Murillou <murillovinicius13@hotmail.com>
2025-03-13 18:47:51 -0300

    refactor: adicionar controllers no front-controller

--- public/index.php
+++ public/index.php
@@ -1,28 +1,44 @@
-<?php 
+<?php
 
+use Phplay\Mvc\Controller\{
+    Controller,
+    DeleteVideoController,
+    UpdateVideoController,
+    Error404Controller,
+    NewVideoController,
+    VideoFormController,
+    VideoListController
+};
+use Phplay\Mvc\Repository\VideoRepository;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
+require __DIR__ . '/../config/connection-bd.php';
+$videoRepository = new VideoRepository($pdo);
 
 if (!array_key_exists('REQUEST_URI', $_SERVER) || 
 strpos($_SERVER['REQUEST_URI'], '/?') === 0 || $_SERVER['REQUEST_URI'] === '/') {
-  require_once __DIR__ . '/../list-of-videos.php';
+    $controller = new VideoListController($videoRepository);
+
 } elseif ($_SERVER['REQUEST_URI'] === '/new-video') {
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        require_once __DIR__ . '/../forms.php';
+        $controller = new VideoFormController($videoRepository);
     } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        require_once __DIR__ .'/../scripts/new-video.php';
+        $controller = new NewVideoController($videoRepository);
     }
 
 } elseif (strpos($_SERVER['REQUEST_URI'], '/update-video') === 0) {
   if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    require_once __DIR__ . '/../forms.php';
+        $controller = new VideoFormController($videoRepository);
 } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    require_once __DIR__ . '/../scripts/update-video.php';
+        $controller = new UpdateVideoController($videoRepository);
 }
 
 } elseif (strpos($_SERVER['REQUEST_URI'], needle: '/delete-video') === 0) {
-    require_once __DIR__ . '/../scripts/delete-video.php';
+        $controller = new DeleteVideoController($videoRepository);
 } else {
-    http_response_code(404);
+        $controller = new Error404Controller();
 }
+
+/** @var Controller $controller */
+$controller->processRequest();
