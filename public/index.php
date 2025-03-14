11a2ed9b4a710aaf3adf82603a0bfe3365e66cde
Murillou <murillovinicius13@hotmail.com>
2025-03-14 18:29:42 -0300

    feat: criar rota para o controller de autenticação

--- public/index.php
+++ public/index.php
@@ -7,14 +7,18 @@ use Phplay\Mvc\Controller\{
     Error404Controller,
     NewVideoController,
     VideoFormController,
-    VideoListController
+    VideoListController,
+    LoginFormController,
+    AuthController
 };
+use Phplay\Mvc\Repository\UsersRepository;
 use Phplay\Mvc\Repository\VideoRepository;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
 require __DIR__ . '/../config/connection-bd.php';
 $videoRepository = new VideoRepository($pdo);
+$userRepository = new UsersRepository($pdo);
 
 if (!array_key_exists('REQUEST_URI', $_SERVER) || 
 strpos($_SERVER['REQUEST_URI'], '/?') === 0 || $_SERVER['REQUEST_URI'] === '/') {
@@ -36,6 +40,13 @@ strpos($_SERVER['REQUEST_URI'], '/?') === 0 || $_SERVER['REQUEST_URI'] === '/') 
 
 } elseif (strpos($_SERVER['REQUEST_URI'], needle: '/delete-video') === 0) {
         $controller = new DeleteVideoController($videoRepository);
+} elseif (strpos( $_SERVER['REQUEST_URI'], '/login') === 0 ){
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $controller = new LoginFormController();
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $controller = new AuthController($userRepository); 
+}
+
 } else {
         $controller = new Error404Controller();
 }
