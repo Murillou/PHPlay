c3f6aa7ab5917ef9b65cb6c1d5f174f82c79d93e
Murillou <murillovinicius13@hotmail.com>
2025-03-16 13:09:05 -0300

    refactor: adicionar file_path a tabela de vídeos

--- src/Model/Video.php
+++ src/Model/Video.php
@@ -6,6 +6,7 @@ class Video
 {
       public readonly int $id;
       public readonly string $url;
+      private ?string $filePath = null;
       public function __construct(
         string $url, 
         public readonly string $title
@@ -23,5 +24,15 @@ class Video
 
       public function setId(int $id): void {
           $this->id = $id;
+      }
+
+      public function setFilePath(string $filePath): void
+      {
+          $this->filePath = $filePath;
+      }
+
+      public function getFilePath(): ?string
+      {
+          return $this->filePath;
       }
 }
