bd867f1dfb2e7cccb46525b6b91189aff7f03ca5
Murillou <murillovinicius13@hotmail.com>
2025-03-16 23:50:41 -0300

    chore: atualizar string de conexão do banco de dados com exemplos claros

--- config/connection-bd.php
+++ config/connection-bd.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-  $pdo = new PDO('mysql:host=localhost;dbname=phplay', 'root', '1258712587');
+  $pdo = new PDO('mysql:host=localhost;dbname=nome-do-seu-banco', 'seu-username', 'sua-senha');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION) ;
   $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
