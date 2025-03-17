eafa71dadab9b0a095ba5583d3529a39b9353476
Murillou <murillovinicius13@hotmail.com>
2025-03-16 23:42:44 -0300

    feat: criar script para criação de usuário

--- create-user.php
+++ create-user.php
@@ -4,10 +4,10 @@ require __DIR__ . '/config/connection-bd.php';
 
 $email = $argv[1];
 $password = $argv[2];
-$hash =  password_hash($password, PASSWORD_ARGON2ID);
+$hash =  password_hash($password, algo: PASSWORD_ARGON2ID);
 
 $sqlQuery = 'INSERT INTO users (email, password) VALUES (:email, :password);';
-$stmt = $pdo->prepare($sqlQuery);
+$stmt = $pdo->prepare(query: $sqlQuery);
 $stmt->bindValue(':email', $email, PDO::PARAM_STR);
 $stmt->bindValue(':password', $hash );
 $stmt->execute();
